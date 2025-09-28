12047d2c38b7208d6567d3d99751f43d544f9264
ayanayvleo <163200339+ayanayvleo@users.noreply.github.com>
2025-09-28 14:57:20 -0500

    Revert to simple credentials assignment - let Torque handle automatically

--- quali_torque_assignment/main.tf
+++ quali_torque_assignment/main.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "example" {
