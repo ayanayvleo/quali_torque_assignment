8de9c4e68088d976ee2aadaef92992afb7e5796d
ayanayvleo <163200339+ayanayvleo@users.noreply.github.com>
2025-09-28 14:51:53 -0500

    Pass AWS credentials explicitly as Terraform variables

--- quali_torque_assignment/variables.tf
+++ quali_torque_assignment/variables.tf
@@ -8,3 +8,15 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
